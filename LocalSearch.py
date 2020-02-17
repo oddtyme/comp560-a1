ef1d781ec4e7b653444626fb7ac9ad3c99c09013
oddtyme <seamus@live.unc.edu>
2020-02-16 23:17:15 -0500

    Added initial random assignment for local search

--- LocalSearch.py
+++ LocalSearch.py
@@ -1,5 +1,27 @@
+# import randrange for random assignment
+from random import randrange
+
 # Class that holds all functions relevant to local search
 class LocalSearch:
+	# main function where action takes place
+	def main(self, adj_list, colors):
+		print("Randomly assigning colors...")
+		state_color_dict = self.randomAssignment(adj_list.keys(), colors)		
+
+		for state in state_color_dict:
+			print("Randomly assigned " + state_color_dict[state] + " to " + state)
+
+
+	# function that randomly assigns all states a color
+	def randomAssignment(self, state_list, colors):
+		state_color_dict = {}
+		
+		for state in state_list:
+			state_color_dict[state] = colors[randrange(4)]
+
+		return state_color_dict
+
+	
 	# function that takes in current solution and evaluates cost
 	def cost(self, adj_list, state_color_dict):
 		# cost is intially 0
