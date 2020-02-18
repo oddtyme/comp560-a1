1976156aa6dcdf777627e592568b5315a5032bb6
oddtyme <seamus@live.unc.edu>
2020-02-17 22:39:30 -0500

    Fixed misconceptions with SA

--- LocalSearch.py
+++ LocalSearch.py
@@ -22,50 +22,60 @@ class LocalSearch:
 
 		print("Searching...")
 
-		while self.cost(adj_list, state_color_dict) > 0:
-			# create potential new solution
-			state_color_dict_prime = dict(state_color_dict)
+		# SA temperature set to 1, min temp to decrease factor to .9, and number of iterations to 100
+		temp = 1
+		min_temp = .0001
+		num_iterations = 100
+		decrease = .9
 
-			# choose random state 
-			rand_state = random.choice(list(state_color_dict))
+		while self.cost(adj_list, state_color_dict) > 0 and temp > min_temp:
+			for x in range(0, num_iterations):
+				# create potential new solution
+				state_color_dict_prime = dict(state_color_dict)
+
+				# choose random state 
+				rand_state = random.choice(list(state_color_dict))
 			
 
-			# create copy of colors
-			available_colors = list(colors)
+				# create copy of colors
+				available_colors = list(colors)
 			
-			# remove original color from list of colors 
-			available_colors.remove(state_color_dict[rand_state])
+				# remove original color from list of colors 
+				available_colors.remove(state_color_dict[rand_state])
 
-			# randomly choose a color from that list and assign it to randomly selected state
-			rand_color = random.choice(available_colors)
-			state_color_dict_prime[rand_state] = rand_color
+				# randomly choose a color from that list and assign it to randomly selected state
+				rand_color = random.choice(available_colors)
+				state_color_dict_prime[rand_state] = rand_color
 
-			# compare solutions
-			if self.cost(adj_list, state_color_dict_prime) < self.cost(adj_list, state_color_dict):
-				print("Cooling...")
-				print("Swtiched " + rand_state + " from " + state_color_dict[rand_state] + " to " + rand_color)
-				state_color_dict = state_color_dict_prime
-			else:
-				# implement simulated annealing condition
-				# calculate difference in temp of trade
-				diff = self.cost(adj_list, state_color_dict_prime) - self.cost(adj_list, state_color_dict)
-
-				# delta d negated
-				negdiff = -1 * diff
-				
-				# exponential value calculated
-				exponent = float(negdiff) / float(self.cost(adj_list, state_color_dict))
-		
-				# SA condition for accepting bad trades
-				if math.exp(exponent) > random.uniform(0, 1):
-					print("Warming...")
+				# compare solutions
+				if self.cost(adj_list, state_color_dict_prime) < self.cost(adj_list, state_color_dict):
+					print("Accepted good result!")
 					print("Swtiched " + rand_state + " from " + state_color_dict[rand_state] + " to " + rand_color)
 					state_color_dict = state_color_dict_prime
-					
+				else:
+					# implement simulated annealing condition
+					# calculate difference in temp of trade
+					diff = self.cost(adj_list, state_color_dict) - self.cost(adj_list, state_color_dict_prime)
+				
+					# exponential value calculated
+					exponent = float(diff) / float(temp)
+		
+					# SA condition for accepting bad trades
+					if math.exp(exponent) > random.uniform(0, 1):
+						print("Accepted bad result!")
+						print("Swtiched " + rand_state + " from " + state_color_dict[rand_state] + " to " + rand_color)
+						state_color_dict = state_color_dict_prime
 
-		print("----------------LOCAL SEARCH RESULTS-------------------")
-		for state in state_color_dict:
-			print("State: " + state + ", Color: " + state_color_dict[state])
+			print("Cooling...")
+			temp = temp * decrease
+					
+		
+		if self.cost(adj_list, state_color_dict) is 0:
+			print("----------------LOCAL SEARCH RESULTS-------------------")
+			for state in state_color_dict:
+				print("State: " + state + ", Color: " + state_color_dict[state])
+		else:
+			print("Algorithm failed to arrive at a solution")
 		
 	
 
